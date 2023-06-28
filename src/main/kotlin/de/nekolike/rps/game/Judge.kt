f1bee095a39a360c03fdb50eff27bab72e84f3a5
Nigel Volkmann <nigel.volkmann1994@googlemail.com>
2023-06-28 23:14:05 +0200

    [refactor] Improve winner determination

--- src/main/kotlin/de/nekolike/rps/game/Judge.kt
+++ src/main/kotlin/de/nekolike/rps/game/Judge.kt
@@ -3,25 +3,16 @@ package de.nekolike.rps.game
 import de.nekolike.rps.model.Action
 
 object Judge {
+    private val rules = mapOf(
+        Action.Rock to Action.Scissors,
+        Action.Paper to Action.Rock,
+        Action.Scissors to Action.Paper
+    )
     fun determineWinnerAndAddResultsToPlayers(player: Player, opponent: Player) {
-        if (playerChoosesRock(player.action)) {
-            when (opponent.action) {
-                Action.Rock -> playerDraws(player, opponent)
-                Action.Paper -> playerLoses(player, opponent)
-                Action.Scissors -> playerWins(player, opponent)
-            }
-        } else if (playerChoosesPaper(player.action)) {
-            when (opponent.action) {
-                Action.Rock -> playerWins(player, opponent)
-                Action.Paper -> playerDraws(player, opponent)
-                Action.Scissors -> playerLoses(player, opponent)
-            }
-        } else if (playerChoosesScissors(player.action)) {
-            when (opponent.action) {
-                Action.Rock -> playerLoses(player, opponent)
-                Action.Paper -> playerWins(player, opponent)
-                Action.Scissors -> playerDraws(player, opponent)
-            }
+        when {
+            player.action == opponent.action -> playerDraws(player, opponent)
+            rules[player.action] == opponent.action -> playerWins(player, opponent)
+            else -> playerLoses(player, opponent)
         }
     }
 
@@ -52,3 +43,4 @@ object Judge {
         opponent.wins++
     }
 }
+
