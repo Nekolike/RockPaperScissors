a8804dc083a20fb81ced772e7c6e46d7171cb82d
Nigel Volkmann <nigel.volkmann1994@googlemail.com>
2023-06-28 23:11:44 +0200

    [feat] Add main function to run game

---
+++ src/main/kotlin/de/nekolike/rps/Main.kt
@@ -0,0 +1,25 @@
+package de.nekolike.rps
+
+import de.nekolike.rps.model.Strategy
+import de.nekolike.rps.game.Player
+
+fun main(args: Array<String>) {
+    val player = Player(Strategy.Random)
+    val opponent = Player(Strategy.Rock)
+
+    repeat(100) {
+        player.playAgainst(opponent)
+    }
+
+    println("--------- Player Stats ---------")
+    println("Player wins: ${player.wins}")
+    println("Player draws: ${player.draws}")
+    println("Player loses: ${player.losses}")
+
+    println("--------- Opponent Stats ---------")
+    println("Opponent wins: ${opponent.wins}")
+    println("Opponent draws: ${opponent.draws}")
+    println("Opponent loses: ${opponent.losses}")
+
+}
+
